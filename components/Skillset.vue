f83d5cd24c5072efa780a694c457f070175fc0e7
Kianmehr Haddad <kianmehr1777@gmail.com>
2024-12-16 22:07:36 -0500

    Update skillset component

--- components/Skillset.vue
+++ components/Skillset.vue
@@ -4,7 +4,7 @@ import { Icon } from '@iconify/vue';
 const skills = ref([
   { id: 1, icon: 'fa6-solid:code', title: "Frontend Development", content: "Transforming designs into interactive experiences, optimized for speed and scalability." },
   { id: 2, icon: 'fa6-solid:server', title: "Backend Development", content: "Building and maintaining robust server-side logic, ensuring data integrity and seamless integration." },
-  { id: 3, icon: 'fa6-solid:sliders', title: "Motion Web Design", content: "Elevating web designs with fluid animations and transitions, enhancing dynamics and user engagement." },
+  { id: 3, icon: 'fa6-solid:chart-line', title: "Data Analytics Solutions", content: "Transforming data into meaningful insights for smarter and informed decision-making."},
   { id: 4, icon: 'iconamoon:synchronize-bold', title: "Automation", content: "Designing scripts and bots to automate repetitive tasks, streamlining processes for efficiency." }
 ]);
 
@@ -58,12 +58,9 @@ onBeforeUnmount(() => {
 <template>
   <div class="relative">
     <div class="grid grid-cols-1 text-center gap-4 sm:grid-cols-2 sm:text-left">
-      <div 
-        v-for="skill in skills" 
-        :key="skill.id" 
-        class="grid-item flex flex-col p-6 border-4 border-transparent rounded-lg items-center sm:items-start relative overflow-hidden"
-      >
-        <Icon :icon="skill.icon" class="text-4xl"/>
+      <div v-for="skill in skills" :key="skill.id"
+        class="grid-item flex flex-col p-6 border-4 border-transparent rounded-lg items-center sm:items-start relative overflow-hidden">
+        <Icon :icon="skill.icon" class="text-4xl" />
         <h2 class="text-lg sm:text-xl font-bold my-2.5">{{ skill.title }}</h2>
         <p class="text-md sm:text-lg text-[--secondary-text-color]">{{ skill.content }}</p>
       </div>
